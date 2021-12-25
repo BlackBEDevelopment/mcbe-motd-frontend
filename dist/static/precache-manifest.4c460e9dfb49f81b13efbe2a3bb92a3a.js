15922c63b77426faf68a999939b1fbe3903132e9
lixworth <lixworth@gmail.com>
2021-12-25 22:55:39 +0800

    generate build file

---
+++ dist/static/precache-manifest.4c460e9dfb49f81b13efbe2a3bb92a3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2e42324608da460947",
+    "url": "static/css/app.ea0769b2.css"
+  },
+  {
+    "revision": "eb2301e541840b851d2b",
+    "url": "static/css/chunk-vendors.fd3523e7.css"
+  },
+  {
+    "revision": "276d82ef8e77ce9ca01adce2984217f5",
+    "url": "static/img/img.276d82ef.png"
+  },
+  {
+    "revision": "8060040e4da8019f8a2408a163d7278a",
+    "url": "static/img/pm.8060040e.png"
+  },
+  {
+    "revision": "380163b7b7f5e916ebf7052d6821beee",
+    "url": "static/index.html"
+  },
+  {
+    "revision": "8f2e42324608da460947",
+    "url": "static/js/app.9460ca05.js"
+  },
+  {
+    "revision": "eb2301e541840b851d2b",
+    "url": "static/js/chunk-vendors.ecb6603b.js"
+  },
+  {
+    "revision": "38969a03bd8c5cfae8f9d567b0b46bec",
+    "url": "static/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "static/robots.txt"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {},
+  publicPath: './static/',
+  // 输出文件目录
+  outputDir: 'dist/static',
 }
